30ff4f99a3001da96e67833bdb3ee1af0d25b80d
John Fuquene <jfuquene@email.wm.edu>
2020-11-06 23:16:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] === "s"
-   "s" * 10 + string 
-    
+   new_string = "s" * 10 + string 
+   binding.pry
+   return new_string 
   else
     string
   end
